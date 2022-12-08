7e4333e49a742c269df2a28fd9500f87b18d4311
jennifer1-tech <jennifer.ifychukwurah@gmail.com>
2022-12-08 08:49:18 -0800

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -13,9 +13,7 @@
 
 
 int main(void)
-  
 {
-  printf("\"Programming is like building a multilingual puzzle");
-  return (0);
-  
+puts("\"Programming is like building a multilingual puzzle");
+return (0);  
 }
