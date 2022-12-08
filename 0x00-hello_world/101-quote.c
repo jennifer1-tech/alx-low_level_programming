7985221c4b6673acd880b05b4535a3b5d3251dd5
jennifer1-tech <jennifer.ifychukwurah@gmail.com>
2022-12-08 13:53:44 -0800

    tast

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - prints exactly and that piece of art is useful
+ *
+ * Return: Always 1 (success)
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
