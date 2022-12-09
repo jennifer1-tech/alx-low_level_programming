da749ec33a41de6a5e8c0ce4732a6dec47fa4ebe
jennifer1-tech <jennifer.ifychukwurah@gmail.com>
2022-12-09 07:44:57 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print numbers
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i =0; i < 10; i++)
+		printf("%d", i);
+
+	printf("\n");
+
+	return (0);
+}
