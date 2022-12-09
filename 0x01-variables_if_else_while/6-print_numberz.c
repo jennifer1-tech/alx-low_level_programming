4e8779a54f6d6129ce2a7da4f880e4a47c561a70
jennifer1-tech <jennifer.ifychukwurah@gmail.com>
2022-12-09 08:34:30 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - 6 print numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int w;
+
+	fort (w = '0'; w <= '9'; w++)
+	{
+		putchar(w);
+	}
+	putchar('w');
+	return (0);
+}
