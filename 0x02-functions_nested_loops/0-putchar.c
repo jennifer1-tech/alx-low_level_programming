e42196e65eea1f8fc927d664a8b1e2b3339bb38c
jennifer1-tech <jennifer.ifychukwurah@gmail.com>
2022-12-13 10:24:33 -0800

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,15 +8,7 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf("_putchar");
 
-	return (0);
+        return (0);
 }
