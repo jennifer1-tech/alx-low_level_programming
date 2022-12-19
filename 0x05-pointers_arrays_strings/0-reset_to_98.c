27d2a3dffbb2f04e00fff8aba3ac489db9f783a4
jennifer1-tech <jennifer.ifychukwurah@gmail.com>
2022-12-19 09:07:26 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
- * reset_to_98 - update the value of n to 98
- * @n: input
- * Return: n
+ * reset_to_98-Resets the value of a variable back to 98
+ * @n:variable to be reset
+ * Return:Void
  */
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,4 +13,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void puts2(char *str);
 int _atoi(char *s);
+
 #endif
