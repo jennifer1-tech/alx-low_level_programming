6b6cb66d3c2da011b17c7e76f069ec620438fead
jennifer1-tech <jennifer.ifychukwurah@gmail.com>
2022-12-21 12:23:44 -0800

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @n: input
+ * Return: the value of n
+ */
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOTlL";
+	char s2[] = "443300711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
+}
