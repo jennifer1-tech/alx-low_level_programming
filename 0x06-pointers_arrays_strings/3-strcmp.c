5f88e95912e5acad61e0275ad6b9ba8a1487812d
jennifer1-tech <jennifer.ifychukwurah@gmail.com>
2022-12-21 11:40:50 -0800

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int _strcmp(char *sl, char *s2)
 	int i;
 
 	i = 0;
-	while (sl[i] != '\0' && s2[i] != '\0')
+	while (s2[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s2[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			return (s2[i] - s2[i]);
 		}
 	i++;
 	}
