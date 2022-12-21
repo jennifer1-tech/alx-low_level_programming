be37b83ef38a97a668d3d67bb9fba940ebaa587c
jennifer1-tech <jennifer.ifychukwurah@gmail.com>
2022-12-21 11:15:03 -0800

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings values
+ * @sl: string 1
+ * @s2: string 2
+ * Return: the difference in sl and s2
+ */
+int _strcmp(char *sl, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (sl[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	i++;
+	}
+	return (0);
+}
