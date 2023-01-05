b9718c05dc2188bf7dda31a140d7d3fded80d4f1
jennifer1-tech <110386652+jennifer1-tech@users.noreply.github.com>
2023-01-04 22:44:11 -0800

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,47 +1,23 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * palind2 - obtains length of a
- * @a: string
- * @l: integer to count length
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int palind2(char *a, int l)
-{
-	if (*a == 0)
-		return (l - 1);
-	return (palind2(a + 1, l + 1));
-}
-/**
- * palind3 - compares string vs string reverse
- * @a: string
- * @l: length
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * is_palindrome - check if the value is palindrome or not
+ * @s: check the string if its palindrome
+ * Return: return 1 if true and 0 if false
  */
 
-int palind3(char *a, int l)
-{
-	if (*a != *(a + l))
-		return (0);
-	else if (*a == 0)
-		return (1);
-	return (palind3(a + 1, l - 2));
-}
-/**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to evaluate
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int is_palindrome(char *s)
 {
-	int l;
+	int length = strlen(s);
 
-	l = palind2(s, 0);
-	return (palind3(s, 1));
+	if (length <= 1)
+	{
+		return (1);
+	}
+	if (s[0] != s[length - 1])
+	{
+		return (0);
+	}
+	return (1);
 }
