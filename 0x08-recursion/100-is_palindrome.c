f0895adef76224acf351f14d889968c9c853de5d
jennifer1-tech <110386652+jennifer1-tech@users.noreply.github.com>
2023-01-05 01:06:43 -0800

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,23 +1,41 @@
-#include "main.h"
-#include <string.h>
+* _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
 
 /**
- * is_palindrome - check if the value is palindrome or not
- * @s: check the string if its palindrome
- * Return: return 1 if true and 0 if false
+ * comparator - compares each character of the string.
+ * @s: string
+ * @n1: smallest iterator.
+ * @n2: biggest iterator.
+ * Return: .
  */
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
 
+/**
+ * is_palindrome - detects if a string is a palindrome.
+ * @s: string.
+ * Return: 1 if s is a palindrome, 0 if not.
+ */
 int is_palindrome(char *s)
 {
-	int length = strlen(s);
-
-	if (length <= 1)
-	{
+	if (*s == '\0')
 		return (1);
-	}
-	if (s[0] != s[length - 1])
-	{
-		return (0);
-	}
-	return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
