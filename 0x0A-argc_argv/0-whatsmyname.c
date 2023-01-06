c6f15ea3ab2d14f286afcdf08f8969508b1629a1
jennifer1-tech <Jennifer.ifychukwurah@gmail.com>
2023-01-06 13:04:04 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - return my name
+ * @argc: count length
+ * @argv: display my name
+ * Return: always 0 Success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
