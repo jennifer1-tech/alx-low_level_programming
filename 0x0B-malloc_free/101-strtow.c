2104eb6909cbd11ddc5420917ce875b41007e60a
jennifer1-tech <Jennifer.ifychukwurah@gmail.com>
2023-01-09 04:57:19 -0800

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,14 +13,13 @@ int count_word(char *s)
 
 	flag = 0;
 	w = 0;
-
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
 			flag = 0;
 		else if (flag == 0)
 		{
-flag = 1;
+			flag = 1;
 			w++;
 		}
 	}
