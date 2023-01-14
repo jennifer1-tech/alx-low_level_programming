dcc22db97bb43750e3ced6a5e9d61e33dcabd1b0
jennifer1-tech <Jennifer.ifychukwurah@gmail.com>
2023-01-14 10:16:54 -0800

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
  * malloc_checked - allocates memory.
  * @b: the size to allocate.
- *
  * Return: pointer to allocated memory or 98 if failed.
  */
 
 void *malloc_checked(unsigned int b)
 {
-	char *ptr;
+	void *ptr;
 
 	ptr = malloc(b);
+
 	if (ptr == NULL)
 		exit(98);
-
 	return (ptr);
-
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
-#define <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+#define MAIN_H
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
