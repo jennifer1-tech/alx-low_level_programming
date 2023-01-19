b4f15efd5396089d9ddc41db72c16eb34f9e20f2
jennifer1-tech <Jennifer.ifychukwurah@gmail.com>
2023-01-18 23:44:57 -0800

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -8,17 +8,10 @@ int op_mod(int a, int b);
 
 /**
  * op_add - Returns the sum of two numbers.
-<<<<<<< HEAD
  *  @a: The first number.
  *  @b: The second number.
  *
  *  Return: The sum of a and b.
-=======
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The sum of a and b.
->>>>>>> 232c21fd43e6e78de563b098d48db76521fd6d75
  */
 int op_add(int a, int b)
 {
@@ -33,12 +26,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-<<<<<<< HEAD
-	return (a * b);
-}
-/**
- * op_mul - Returns the remainder of the division of two numbers.
-=======
+
 	return (a - b);
 }
 /**
@@ -65,17 +53,12 @@ int op_div(int a, int b)
 }
 /**
  * op_mod - Returns the remainder of the division of two numbers.
->>>>>>> 232c21fd43e6e78de563b098d48db76521fd6d75
  * @a: The first number.
  * @b: The second number.
  *
  * Return: The remainder of the division of a by b.
  */
-<<<<<<< HEAD
-int op_mul(int a, int b)
-=======
 int op_mod(int a, int b)
->>>>>>> 232c21fd43e6e78de563b098d48db76521fd6d75
 {
 	return (a % b);
 }
