a0654aceb3c728f22a39dbdc0fd7d7153588eec5
jennifer1-tech <jennifer.ifychukwurah@gmail.com>
2023-03-21 02:29:18 -0700

    task 3

---
+++ 0x18-dynamic_libraries/100-operation.c
@@ -0,0 +1,49 @@
+include <stdio.h>
+
+/**
+ * add - addition
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substitution
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multipication
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+int mod(int a, int b)
+{
+	return (a % b);
+}
