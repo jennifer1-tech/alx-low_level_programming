dbc6a21b3c950ec4d7b1bdebb63b68c134222666
jennifer1-tech <jennifer.ifychukwurah@gmail.com>
2023-03-21 02:44:34 -0700

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
-Paste this inside.
 #!/bin/bash
 wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so"
